f65369c56ae571a270aebc82216ce0326e4d7b57
akash142005 <akashanbarasuz@gmail.com>
2026-06-01 14:51:23 +0530

    2 D Array Using binary Search

---
+++ SortedArray2DBS.java
@@ -0,0 +1,69 @@
+import java.util.Arrays ;
+public class SortedArray2DBS {
+  public static void main(String[] args) {
+    int [][] matrix = {
+      {1,2,3},
+      {4,5,6} ,
+      {7,8,9}
+    } ;
+
+    int ans [] = searchSpace(matrix, 5) ;
+
+    System.out.print(Arrays.toString(ans));
+  }
+
+  public static int [] binarySearch(int matrix [][] ,int row ,int cStart ,int cEnd ,int target){
+    while(cStart <= cEnd){
+      int mid = cStart + (cEnd -cStart)/2 ;
+      if(matrix[row][mid] == target){
+          return new int [] {row ,mid} ;
+      }
+      if (matrix [row][mid] > target){
+        cEnd = mid - 1 ;
+      }
+      else {
+        cStart = mid + 1 ;
+      }
+    }
+    return new int[] {-1 ,-1};
+  }
+
+  public static int[] searchSpace(int [][] matrix , int target){
+      int row = matrix.length ;
+      int col = matrix[0].length ;
+
+      if (row == 1){
+        return binarySearch(matrix, row, 0 , col -1, target) ;
+      }
+      int rStart = 0 ;
+      int rEnd = row - 1 ;
+      int cMid = col / 2 ;
+      while(rStart < (rEnd - 1)){
+        int mid = rStart + (rEnd - rStart) / 2 ;
+          if(matrix[mid][cMid] == target){
+              return new int []{mid , cMid};
+          }
+
+          if(matrix[mid][cMid] > target){
+              rEnd = mid ;
+          }
+          else {
+            rStart = mid ;
+          }
+      }
+
+      if(matrix[rStart][cMid] >= target){
+         return binarySearch(matrix, rStart, 0, cMid - 1, target) ;
+      }
+      else if (matrix[rStart][cMid] <= target && matrix[rStart][col -1] >= target){
+         return binarySearch(matrix, rStart, cMid + 1, col -1 , target) ;
+      }
+      else if (matrix[rStart + 1][cMid] >= target){
+         return binarySearch(matrix, rStart + 1, 0, cMid - 1 , target) ;
+      }
+      else{
+        return binarySearch(matrix, rStart + 1, cMid + 1, col -1 , target) ;
+      }
+    }
+
+}
