d94e478611a33280c8f75a7ca94898706417206c
akash142005 <akashanbarasuz@gmail.com>
2026-06-02 17:44:43 +0530

    solve Questions in cyclic Sort

---
+++ FindMissingElement.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindMissingElement {
+    public static void main(String[] args) {
+        int [] nums = {5 ,2 ,1 ,2 ,3 ,6 ,7 ,5} ;
+        List <Integer> ans = findDisappearedNumbers(nums) ;
+        System.out.print(ans);
+
+    }
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0 ;
+        while(i < nums.length){
+            int correct = nums[i] - 1 ;
+            if(nums[i] <= nums.length && nums[i] != nums[correct]){
+                swap(nums , i , correct) ;
+            }
+            else{
+                i++ ;
+            }
+        }
+
+            List<Integer> ans =new ArrayList<>();
+        for(int j =0 ;j < nums.length ;j++){
+            if(nums[j] != j + 1){
+                ans.add(j + 1);
+            }
+        }
+        return ans ;
+    }
+    public static void swap(int [] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second] ;
+        arr[second] = temp ;
+    }
+
+}
