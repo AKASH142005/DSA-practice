ca7ded30c46611d60647cd0d23065918822989b3
akash142005 <akashanbarasuz@gmail.com>
2026-06-05 20:28:29 +0530

    check

--- pattern/NumberRectangle.java
+++ pattern/NumberRectangle.java
@@ -6,7 +6,7 @@ public class NumberRectangle {
           int n = 4 ;
          int OriginalN = n ;
          int N = 2 * n ;
-
+//
          for(int row = 1; row < N; row++){
             for(int col = 1 ; col < N ;col++){
                 int ArrayValue =OriginalN - Math.min(Math.min(row ,col) ,Math.min(N-row ,N-col)) + 1;
