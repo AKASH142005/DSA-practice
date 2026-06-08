913ea4285c75f0332ddb124065bc64092e82cf8d
akash142005 <akashanbarasuz@gmail.com>
2026-06-08 20:48:35 +0530

    In Bitwise Odd or even using and operator

---
+++ Bitwise/OddOrEven.java
@@ -0,0 +1,14 @@
+package Bitwise;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int n = 2;
+
+        if((n & 1) == 1){
+            System.out.println("Odd");
+        }
+        else{
+            System.out.println("Even");
+        }
+    }
+}
