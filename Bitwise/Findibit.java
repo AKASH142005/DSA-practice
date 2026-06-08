e000dee81fb233e14cebb156e828f6a0f9cd5eaf
akash142005 <akashanbarasuz@gmail.com>
2026-06-08 21:17:28 +0530

    using bitwise operator find the i position of the bit in the integer

--- Bitwise/Findibit.java
+++ Bitwise/Findibit.java
@@ -2,7 +2,14 @@ package Bitwise;
 
 public class Findibit {
     public static void main(String[] args) {
-        
+        int pos = 4;
+        int n = 12 ;
+
+        if ((n & (1 << (pos - 1))) != 0) {
+            System.out.println("Bit is 1");
+        } else {
+            System.out.println("Bit is 0");
+        }
     }
     
 }
