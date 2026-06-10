300ff466cf11139478c2410d3e8e01087aec64d2
akash142005 <akashanbarasuz@gmail.com>
2026-06-10 11:45:54 +0530

    Find the number digit in the bits using the log operation

---
+++ Bitwise/NoDigits.java
@@ -0,0 +1,9 @@
+package Bitwise;
+
+public class NoDigits {
+    public static void main(String[] args) {
+        int n = 10;
+        int b = 2 ;
+    System.out.println((int)(Math.log(n) /Math.log(b)) + 1);
+    } 
+}
