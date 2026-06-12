e677da797f39d2b0299e6051f3b99b56c68e4748
akash142005 <akashanbarasuz@gmail.com>
2026-06-12 18:32:16 +0530

    Find the lcm and gcd using the algorithm

--- Math/LcmGcd.java
+++ Math/LcmGcd.java
@@ -1,3 +1,21 @@
+package Math ;
+
 public class LcmGcd {
-    
+    public static void main(String[] args) {
+        int a = 15 ;
+        int b = 20 ;
+        int gcd = GCD(a, b) ;
+        System.out.println(gcd);
+        System.out.println(LCM(a, b, gcd));
+    }
+    public static int GCD(int a ,int b){
+        if(a == 0){
+            return b ;
+        }
+         return GCD(b % a ,a);
+    }
+    public static int  LCM(int a ,int b ,int gcd){
+        int ans = a*b/gcd ;
+        return ans;
+    }
 }
