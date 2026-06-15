8c32ba0ce9252808b7d7c85eb80a2c2622ba3066
akash142005 <akashanbarasuz@gmail.com>
2026-06-15 21:28:29 +0530

    learn quick sort

---
+++ RecursionLevel1/QuickSort.java
@@ -0,0 +1,38 @@
+package RecursionLevel1;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int [] nums = {4,3,6,7,9,1,10} ;
+
+        quickSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void quickSort(int [] nums , int low ,int high ){
+        if(low >= high){
+            return ;
+        }
+        int s = low ;
+        int e = high ;
+        int m = s + (e - s)/ 2;
+        int pivot = nums[m];
+        while (s <= e){
+            while(pivot > nums[s]){
+                s++ ;
+            }
+            while(pivot < nums[e]){
+                e-- ;
+            }
+            if(s <= e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp ;
+                s++ ;
+                e-- ;
+            }
+        }
+        quickSort(nums, low, e);
+        quickSort(nums, s, high);
+    }
+}
