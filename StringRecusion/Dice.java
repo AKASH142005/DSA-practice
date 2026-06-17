1efaee33d2495f84d0db8ed69cdc57e33bbe2927
akash142005 <akashanbarasuz@gmail.com>
2026-06-17 13:33:09 +0530

    how many combination make in the dice for given number

---
+++ StringRecusion/Dice.java
@@ -0,0 +1,34 @@
+package StringRecusion;
+import java.util.ArrayList;
+
+public class Dice {
+    public static void main(String[] args) {
+        int target = 4 ;
+        dice("", target);
+        System.out.print(diceList("", target));
+    }
+    public static void dice(String p , int target){
+
+        if(target == 0){
+            System.out.println(p);
+            return ;
+        }
+
+        for(int i = 1 ; i <= 6 && i <= target;i++){
+             dice(p+i, target-i);
+        }
+    }
+       public static ArrayList<String> diceList(String p , int target){
+
+        if(target == 0){
+            ArrayList <String> list = new ArrayList<>() ;
+            list.add(p);
+            return list;
+        }
+        ArrayList <String> list = new ArrayList<>() ;
+        for(int i = 1 ; i <= 6 && i <= target;i++){
+             list.addAll(diceList(p+i, target-i));
+        }
+        return list ;
+    }
+}
