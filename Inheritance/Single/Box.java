cdfbf0d0ebd560bffbc19e50b7af9e9550395490
akash142005 <akashanbarasuz@gmail.com>
2026-06-20 20:40:44 +0530

    Create a box class for learn inheritance in the oops

---
+++ Inheritance/Single/Box.java
@@ -0,0 +1,19 @@
+package Inheritance.Single;
+
+public class Box {
+    int l ; 
+    int h ;
+    int w ;
+
+    Box(){
+        this.l = -1 ;
+        this.h = -1 ;
+        this.w = -1 ;
+    }
+     Box(int l , int h ,int w){
+        this.l = l ;
+        this.h = h ;
+        this.w = w ;
+    }
+
+}
