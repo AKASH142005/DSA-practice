b954ba1a4c85d2936958b69c98b569347a537e97
akash142005 <akashanbarasuz@gmail.com>
2026-06-21 21:01:21 +0530

    Modify

--- Inheritance/Multiple/Shapes.java
+++ Inheritance/Multiple/Shapes.java
@@ -3,15 +3,12 @@ package Inheritance.Multiple ;
 public class Shapes {
     int length ;
     int height ;
-    int weight ;
-    Shapes(int length ,int height ,int weight){
+    Shapes(int length ,int height ){
         this.length = length ;
         this.height = height ;
-        this.weight = weight ;
     }
     Shapes(){
         this.length =  -1 ;
         this.height =  -1 ;
-        this.weight =  -1 ;
     }
 } 
