b79c2506229fdeacc3096b80ec73be722c7ac6f4
akash142005 <akashanbarasuz@gmail.com>
2026-06-21 20:40:09 +0530

    Create multiple inheritance concept

---
+++ Inheritance/Multiple/Shapes.java
@@ -0,0 +1,17 @@
+package Inheritance.Multiple ;
+
+public class Shapes {
+    int length ;
+    int height ;
+    int weight ;
+    Shapes(int length ,int height ,int weight){
+        this.length = length ;
+        this.height = height ;
+        this.weight = weight ;
+    }
+    Shapes(){
+        this.length =  -1 ;
+        this.height =  -1 ;
+        this.weight =  -1 ;
+    }
+} 
