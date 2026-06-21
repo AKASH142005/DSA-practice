ba4318067bf57accc04cbc465010bc176a1ebce7
akash142005 <akashanbarasuz@gmail.com>
2026-06-21 21:01:12 +0530

    Control by main class

---
+++ Inheritance/Multiple/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.Multiple;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shapes =new Shapes() ;
+        System.out.println(shapes.height) ;
+         Box box =new Box() ;
+        System.out.println(box.height); 
+    }
+}
