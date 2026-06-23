b8ec6aa5ca6d5912b2b439da470586e2da061935
akash142005 <akashanbarasuz@gmail.com>
2026-06-23 14:05:27 +0530

    Create main function to access the data

---
+++ Interface/Main.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public class Main {
+    public static void main(String[] args) {
+      //  Car car = new Car() ;
+      //  car.start();
+       // car.acc();
+       // car.brake();
+        Media player = new Car();
+        player.start(); // engine start and stop solve this problem
+        player.stop();
+    }
+}
