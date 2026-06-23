5ac633c5528beb137065c15dae41c9fb60ea2a46
akash142005 <akashanbarasuz@gmail.com>
2026-06-23 15:40:36 +0530

    add player class to put the function body

---
+++ Interface/CDPlayer.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public class CDPlayer implements Media{
+    @Override
+    public void start(){
+        System.out.println("Music Start");
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Stop Music");
+    }
+}
